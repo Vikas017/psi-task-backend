edf9c53a580be2b43364759af3b3124cfb6fb11d
vikas <vk98659@gmail.com>
2026-04-17 00:21:26 +0530

    mogo error commit

--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
+const PORT = process.env.PORT || 5000;
+import mongoose from "mongoose";
 
+if (!process.env.MONGO_URI) {
+  console.error("❌ MONGO_URI missing");
+  process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ DB connected"))
+  .catch(err => {
+    console.error("❌ DB error:", err.message);
+    process.exit(1);
+  });
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on ${PORT}`);
+});
+/*
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DB connected");
     app.listen(5000, () => console.log("Server running"));
   })
-  .catch(err => console.log(err));
+  .catch(err => console.log(err));*/
